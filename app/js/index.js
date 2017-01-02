5093286e1f448dadfda025c2a075e95254358add
Flying <riafan@hotmail.com>
2017-01-02 22:29:16 +0800

    下拉菜单选中

--- app/js/index.js
+++ app/js/index.js
@@ -22,4 +22,17 @@ $(function() {
       }, 1000)
     }
   });
+
+  $(document.body).on('click', '.dropdown-menu li', function(event) {
+    var target = $(event.currentTarget);
+    target.closest('.dropdown')
+      .find('[data-bind="label"]').text(target.text()).attr('title', target.text())
+      .children('.dropdown-toggle').dropdown('toggle');
+  });
+
+  $('.dropdown-menu li').each(function() {
+    if ($(this).attr('selected')) {
+      $(this).trigger('click');
+    }
+  });
 });
